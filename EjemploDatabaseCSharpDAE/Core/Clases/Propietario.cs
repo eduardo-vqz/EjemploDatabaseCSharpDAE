132ae13d4c7f8d6fc3992dbfb0b85ad94c89e235
eduardo-vqz <eduardo.vqz968@gmail.com>
2025-09-29 11:55:58 -0600

    Entidad Propietario

---
+++ EjemploDatabaseCSharpDAE/Core/Clases/Propietario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploDatabaseCSharpDAE.Core.Clases
+{
+    public class Propietario
+    {
+        private int idPropietario;  // PK
+        private string nombre;   // NOT NULL
+        private string apellido;     // NOT NULL
+        private string dUI;   // UNIQUE, NOT NULL (formato 00000000-0)
+        private string telefono;    // NULL
+        private string direccion;    // NULL
+
+        public int IdPropietario { get => idPropietario; set => idPropietario = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string DUI { get => dUI; set => dUI = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+    }
+}
--- EjemploDatabaseCSharpDAE/obj/Debug/net8.0-windows/EjemploDatabaseCSharpDAE.AssemblyInfo.cs
+++ EjemploDatabaseCSharpDAE/obj/Debug/net8.0-windows/EjemploDatabaseCSharpDAE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjemploDatabaseCSharpDAE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e08ae74923edc76b15a52ed6b9b44f86ef044591")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7511a4a1352a4d8f36ddd99d309aea85960a544")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjemploDatabaseCSharpDAE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjemploDatabaseCSharpDAE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
